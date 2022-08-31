50130df8c206b37346fa7957a55b2d7d4945ed12
Igor_Romanov <igorromanov175@gmail.com>
2022-08-31 16:00:34 +0300

    add Configuration.headless = true

--- src/main/java/com/epam/framework/driver/DriverSingleton.java
+++ src/main/java/com/epam/framework/driver/DriverSingleton.java
@@ -2,9 +2,6 @@ package com.epam.framework.driver;
 
 import com.codeborne.selenide.Configuration;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.Objects;
 
@@ -16,21 +13,21 @@ public class DriverSingleton {
             Configuration.browser = "edge";
             Configuration.driverManagerEnabled = true;
             Configuration.browserSize = "1920x1080";
-            Configuration.headless = false;
+            Configuration.headless = true;
 
         } else if(Objects.equals(System.getProperty("browser"), "firefox") || browser.equals("firefox")){
             WebDriverManager.firefoxdriver().setup();
             Configuration.browser = "firefox";
             Configuration.driverManagerEnabled = true;
             Configuration.browserSize = "1920x1080";
-            Configuration.headless = false;
+            Configuration.headless = true;
 
         } else if (Objects.equals(System.getProperty("browser"), "chrome") || browser.equals("chrome")) {
             WebDriverManager.chromedriver().setup();
             Configuration.browser = "chrome";
             Configuration.driverManagerEnabled = true;
             Configuration.browserSize = "1920x1080";
-            Configuration.headless = false;
+            Configuration.headless = true;
         }
     }
 
